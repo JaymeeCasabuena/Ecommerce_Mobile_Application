57cd50c3ba7fa0cb3837f37e6aeb06fd5a95640b
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-05-20 00:57:48 +1000

    Final Submission -  Changed drawer navigation to bottom tab navigation

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import "react-native-gesture-handler";
 import { StyleSheet } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createStackNavigator } from "@react-navigation/stack";
-import Home from "./src/screens/Home";
-import ProductList from "./src/screens/ProductList";
-import ProductDetails from "./src/screens/ProductDetail";
 import { useFonts } from "expo-font";
 import { useCallback } from "react";
-import { Colors } from "./src/constants/Colors";
-import CustomNavIcon from "./src/components/CustomNavIcons";
 import { Provider } from "react-redux";
 import { store } from "./src/redux/Store";
-import OrderCart from "./src/screens/Cart";
-import CustomDrawerContent from "./src/components/CustomDrawerContent";
-
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+import StackNavigator from "./src/navigation/StackNavigation";
 
 export default function App() {
   SplashScreen.preventAutoHideAsync();
@@ -42,99 +30,9 @@ export default function App() {
     return null;
   }
 
-  const MainNavigator = () => {
-    return (
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ProductList" component={ProductList} />
-        <Stack.Screen name="Product Details" component={ProductDetails} />
-        <Stack.Screen name="Order Cart" component={OrderCart} />
-      </Stack.Navigator>
-    );
-  };
-
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Drawer.Navigator
-          drawerContent={(props) => <CustomDrawerContent {...props} />}
-          initialRouteName="Home"
-          screenOptions={({ navigation, route }) => ({
-            headerLeft: () => {
-              return route.name === "Order Cart" ? (
-                <CustomNavIcon
-                  onPress={navigation.toggleDrawer}
-                  iconName={"arrow-left"}
-                  color={Colors.Peach}
-                  size={26}
-                  style={{ marginRight: -10}}
-                />
-              ) : (
-                <CustomNavIcon
-                  onPress={navigation.toggleDrawer}
-                  iconName={"menu"}
-                  color={Colors.Peach}
-                  size={30}
-                />
-              );
-            },
-            headerRight: () => {
-              return route.name !== "Order Cart" ? (
-                <CustomNavIcon
-                  iconName={"shopping-bag"}
-                  color={Colors.Peach}
-                  size={30}
-                />
-              ) : null
-            },
-            drawerStyle: {
-              backgroundColor: Colors.GrayishWhite,
-            },
-          })}
-        >
-          <Drawer.Screen
-            name="Main"
-            component={MainNavigator}
-            options={{
-              headerShown: true,
-              headerTitle: "Fake Store",
-              headerStyle: {
-                backgroundColor: Colors.DarkestBlue,
-              },
-              headerTitleAlign: "center",
-              headerTintColor: "white",
-              headerTitleStyle: {
-                textTransform: "uppercase",
-                fontFamily: "Lato-Bold",
-                fontSize: 16,
-                letterSpacing: 4,
-              },
-            }}
-          />
-          <Drawer.Screen
-            name="Order Cart"
-            component={OrderCart}
-            options={{
-              title: "Continue Shopping",
-              headerStyle: {
-                backgroundColor: Colors.DarkestBlue,
-              },
-              headerTitleAlign: "left",
-              headerTintColor: "white",
-              headerTitleStyle: {
-                textTransform: "uppercase",
-                fontFamily: "Lato-Bold",
-                fontSize: 14,
-                letterSpacing: 2,
-              },
-            }}
-          />
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <StackNavigator/>
     </Provider>
   );
 }
---
+++ src/navigation/StackNavigation.js
@@ -0,0 +1,180 @@
+import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CustomNavIcon from "../components/CustomNavIcons";
+import OrderCart from "../screens/Cart";
+import Home from "../screens/Home";
+import ProductList from "../screens/ProductList";
+import SignIn from "../screens/Login";
+import SignUp from "../screens/RegisterUser";
+import ProductDetails from "../screens/ProductDetail";
+import UserProfile from "../screens/Profile";
+import { Colors } from "../constants/Colors";
+import Entypo from "@expo/vector-icons/Entypo";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import EvilIcons from "@expo/vector-icons/EvilIcons";
+import FontAwesome from "@expo/vector-icons/FontAwesome6";
+import { useSelector } from "react-redux";
+
+const StackNavigator = () => {
+  const Stack = createStackNavigator();
+  const Tab = createBottomTabNavigator();
+  const { totalItems } = useSelector((state) => state.cart);
+
+  function BottomTabs() {
+    return (
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerShown: true,
+              headerTitle: "Fake Store",
+              headerStyle: {
+                backgroundColor: Colors.DarkestBlue,
+              },
+              headerTitleAlign: "center",
+              headerTintColor: "white",
+              headerTitleStyle: {
+                textTransform: "uppercase",
+                fontFamily: "Lato-Bold",
+                fontSize: 16,
+                letterSpacing: 4,
+              },
+              tabBarShowLabel: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <FontAwesome
+                    name="florin-sign"
+                    size={22}
+                    color={Colors.Peach}
+                  />
+                ) : (
+                  <FontAwesome
+                    name="florin-sign"
+                    size={22}
+                    color={Colors.Gray}
+                  />
+                ),
+            }}
+          />
+          <Tab.Screen
+            name="Products"
+            component={Home}
+            options={{
+              tabBarShowLabel: false,
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <FontAwesome
+                    name="magnifying-glass"
+                    size={22}
+                    color={Colors.Peach}
+                  />
+                ) : (
+                  <FontAwesome
+                    name="magnifying-glass"
+                    size={22}
+                    color={Colors.Gray}
+                  />
+                ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={OrderCart}
+            options={{
+              tabBarShowLabel: false,
+              headerShown: false,
+              tabBarBadge: totalItems ? totalItems : null,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <FontAwesome
+                    name="bag-shopping"
+                    size={24}
+                    color={Colors.Peach}
+                  />
+                ) : (
+                  <FontAwesome
+                    name="bag-shopping"
+                    size={24}
+                    color={Colors.Gray}
+                  />
+                ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={UserProfile}
+            options={{
+              tabBarShowLabel: false,
+              headerShown: false,
+              tabBarIcon: ({ focused }) =>
+                focused ? (
+                  <FontAwesome
+                    name="grip-lines"
+                    size={24}
+                    color={Colors.Peach}
+                  />
+                ) : (
+                  <FontAwesome
+                    name="grip-lines"
+                    size={24}
+                    color={Colors.Gray}
+                  />
+                ),
+            }}
+          />
+        </Tab.Navigator>
+    );
+  }
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Main"
+          component={BottomTabs}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="ProductList"
+          component={ProductList}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Product Details"
+          component={ProductDetails}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Sign up"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  badge: {
+    position: "absolute",
+    right: -10,
+    top: -10,
+    backgroundColor: Colors.Red,
+    width: 20,
+    fontSize: 14,
+    borderRadius: 50,
+    textAlign: "center",
+    paddingBottom: 2,
+    color: Colors.White,
+  },
+});
+
+export default StackNavigator;
